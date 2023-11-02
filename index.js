5372c29136861da69f1164298a7c26f998cf62e7
VilliLiukuri <wrightfredfrik@gmail.com>
2023-11-02 13:04:41 +0200

    uibuild

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
-app.use(express.static('dist'))
 app.use(express.json())
+app.use(express.static('dist'))
+
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
